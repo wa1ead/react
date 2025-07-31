3c3eeee42a6c41ab63e544b01dc3466b40ec9927
wa1ead <waliddraa2@gmail.com>
2025-07-31 10:28:15 +0100

    Create a Header component and import it into the App

---
+++ tuesday/src/components/Logo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Logo.css";
+
+function Logo() {
+  return (
+    <img
+      src="https://store-images.s-microsoft.com/image/apps.30645.9007199266245907.cb06f1f9-9154-408e-b4ef-d19f2325893b.ac3b465e-4384-42a8-9142-901c0405e1bc"
+      alt=""
+      width="60"
+      height="60"
+    />
+  );
+}
+
+export default Logo;
---
+++ tuesday/src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
