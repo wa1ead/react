e67203e06d4f06b6ef60408ee3c10bd30e5c988f
wa1ead <waliddraa2@gmail.com>
2025-08-28 11:33:58 +0100

    input form events

--- tuesday/src/App.jsx
+++ tuesday/src/App.jsx
@@ -1,30 +1,26 @@
-import Header from "./components/Header";
-import CardsContainer from "./components/CardsContainer";
+import { useState } from "react";
 
 function App() {
-  let cardData = [
-    {
-      title: "Twitter",
-      source:
-        "https://store-images.s-microsoft.com/image/apps.26737.9007199266244427.c75d2ced-a383-40dc-babd-1ad2ceb13c86.ed1d047e-03d9-4cd8-a342-c4ade1e58951",
-    },
-    {
-      title: "Instagram",
-      source:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png",
-    },
-    {
-      title: "Youtube",
-      source:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKfPimrl1lwgrW3OHOuuyReVPqtc_Hd7mp4A&s",
-    },
-  ];
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    setText(e.target.value); // capture input value
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // prevent page reload
+    if (text === "walid") {
+      alert(`Submitted: ${text}`);
+    } else {
+      alert("error, machi walid");
+    }
+  };
 
   return (
-    <div>
-      <Header />
-      <CardsContainer cardData={cardData} />
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={text} onChange={handleChange} />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
